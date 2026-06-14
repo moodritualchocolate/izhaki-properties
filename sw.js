042616f9d94e7c5eefa3b868dbd6c927e3f1c8e6
moodritualchocolate <nadav.izhaki89@gmail.com>
2026-06-14 13:33:42 +0300

    Final: deploy final index.html, bump SW cache to v7 (force client refresh)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gal-properties-final-v6';
+const CACHE = 'gal-properties-final-v7';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
